6df6b3636b24b42a59d51f94aa745ad203b620bf
kelogub <66334768+kelogub@users.noreply.github.com>
2020-10-24 06:46:42 +0800

    Add files via upload

--- lesson2010/src/work/Main.java
+++ lesson2010/src/work/Main.java
@@ -1,11 +1,13 @@
 package work;
 
 import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 
 public class Main {
 
     public static void main(String[] args) throws IllegalArgumentException {
-        try (BufferedReader fin = new BufferedReader(new FileReader(args[0]))) {
+        try (BufferedReader fin = new BufferedReader(new FileReader(args[0], StandardCharsets.UTF_8))) {
             String line;
             int count = 0;
             while ((line = fin.readLine()) != null) {
